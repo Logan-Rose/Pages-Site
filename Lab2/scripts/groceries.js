059d0ec02bc23544d1e46582a1ca6886a492169f
Logan-Rose <logan.rose0329@gmail.com>
2021-05-30 14:20:31 -0400

    added Lab2

---
+++ Lab2/scripts/groceries.js
@@ -0,0 +1,118 @@
+	
+// Array of products, each product is an object with different fieldset
+// A set of ingredients should be added to products		 
+
+var products = [
+	{
+		name: "yogurt",
+		lactoseFree: false,
+		nutFree: true,
+		organic: true,
+		price: 1.99
+	},
+	{
+		name: "almond granola",
+		lactoseFree: true,
+		nutFree: false,
+		organic: true,
+		price: 2.35
+	},
+	{
+		name: "salmon",
+		lactoseFree: true,
+		nutFree: true,
+		organic: true,
+		price: 10.00
+	},
+	{
+		name: "Peanut Butter",
+		lactoseFree: true,
+		nutFree: false,
+		organic: false,
+		price: 4.00
+	},
+	{
+		name: "steak",
+		lactoseFree: true,
+		nutFree: true,
+		organic: true,
+		price: 8.00
+	},
+	{
+		name: "Trail Mix",
+		lactoseFree: true,
+		nutFree: false,
+		organic: false,
+		price: 6.00
+	},
+	{
+		name: "Whipped Cream",
+		lactoseFree: false,
+		nutFree: true,
+		organic: false,
+		price: 9.00
+	},
+	{
+		name: "olive oil",
+		lactoseFree: true,
+		nutFree: true,
+		organic: false,
+		price: 10.00
+	},
+	{
+		name: "Cheerios",
+		lactoseFree: true,
+		nutFree: false,
+		organic: false,
+		price: 7.50
+	},
+	{
+		name: "Almond Milk",
+		lactoseFree: true,
+		nutFree: false,
+		organic: true,
+		price: 4.00
+	}
+
+];
+
+
+	
+
+
+// given restrictions provided, make a reduced list of products
+// prices should be included in this list, as well as a sort based on price
+
+function restrictListProducts(prods, restriction, organic) {
+	let returnProds = [];
+	for (let i=0; i<prods.length; i+=1) {
+		if ((restriction == "LactoseIntolerant") && (prods[i].lactoseFree == true)){
+			if( (organic && prods[i].organic) || (!organic) ){
+				returnProds.push(prods[i]);
+			}
+			
+		}
+		else if ((restriction == "NutFree") && (prods[i].nutFree == true)){
+			if( (organic && prods[i].organic) || (!organic) ){
+				returnProds.push(prods[i]);
+			}
+		}
+		else if (restriction == "None"){
+			if( (organic && prods[i].organic) || (!organic) ){
+				returnProds.push(prods[i]);
+			}
+		}
+	}
+	return returnProds;
+}
+
+// Calculate the total price of items, with received parameter being a list of products
+function getTotalPrice(chosenProducts) {
+	totalPrice = 0;
+	for (let i=0; i<products.length; i+=1) {
+		if (chosenProducts.indexOf(products[i].name) > -1){
+			totalPrice += products[i].price;
+		}
+	}
+	return totalPrice;
+}
